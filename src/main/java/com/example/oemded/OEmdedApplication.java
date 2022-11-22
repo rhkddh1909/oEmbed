0f9202e3cf580c2f521fd87c49f273e88361b4cb
이광오 <rhkddh1909@gmail.com>
2022-11-22 22:30:39 +0900

    2022 11 22 프로젝트 생성

---
+++ src/main/java/com/example/oemded/OEmdedApplication.java
@@ -0,0 +1,13 @@
+package com.example.oemded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OEmdedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OEmdedApplication.class, args);
+    }
+
+}
