e049c2230e05633b55e6349af544dde8f10a04cb
rhkddh1909 <rhkddh1909@gmail.com>
2022-11-23 18:00:31 +0900

    2022 11 23 프로젝트 구성

---
+++ src/main/java/com/example/oemded/util/UrlUtil.java
@@ -0,0 +1,5 @@
+package com.example.oemded.util;
+
+public class UrlUtil {
+
+}
---
+++ src/test/java/com/example/oemded/util/UrlUtilTest.java
@@ -0,0 +1,7 @@
+package com.example.oemded.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UrlUtilTest {
+
+}
